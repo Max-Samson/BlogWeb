199a1a55f08fd56dcb91c979e45fa408c98733a8
Max-ShenSHUAI <1809491420@qq.com>
2026-01-30 22:54:04 +0800

    fix: 调试留言功能跑通，pusher成功连接使用，neon正常存储数据

--- src/pages/api/chat/messages.ts
+++ src/pages/api/chat/messages.ts
@@ -4,7 +4,7 @@ import pusher from "@/lib/pusher";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method === "GET") {
     try {
