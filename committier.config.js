652333773c962351db7b9d9e185b76ab72f2c4f3
Max-Samson <364475182@qq.com>
2026-02-01 00:12:18 +0800

    🎨 chore: 修改committtier配置，不显示提交作用域

--- committier.config.js
+++ committier.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from "committier";
 export default defineConfig({
   // 关闭自动emoji（默认开启）
   autoEmoji: true,
-  // 自动从工作区包名推断作用域
-  autoScope: "replaceToPackageName",
+  // 不自动添加作用域，手动输入
+  autoScope: false,
   // 修改默认提交类型为feat
   defaultType: "feat",
   // 开启自动生成默认描述信息来兜底
