d32320038b5f8263d6aa43cd3a1fee25df4964d6
Max-Samson <364475182@qq.com>
2026-02-01 00:18:16 +0800

    🐛 fix: work图片错误显示

--- src/data/works.ts
+++ src/data/works.ts
@@ -22,7 +22,7 @@ export const worksData: WorkItem[] = [
     title: "wuxian's web",
     description:
       "基于Next.js开发的个人介绍网站，简单介绍了我自己个儿，歌和视频都很有品！！！！。",
-    image: "/images/work1.jpg",
+    image: "/images/work1.png",
     tech: ["Next.js", "React", "TypeScript", "Tailwind CSS"],
     link: "#",
     features: ["个人介绍", "作品集", "喜好", "留言"],
